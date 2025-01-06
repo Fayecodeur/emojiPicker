a92f83b2a1e4a108e3c57363074298bdb349e07f
Fayecodeur <fayecodeur99@gmail.com>
2025-01-06 15:51:14 +0000

    creation du formulaire

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,5 +1,18 @@
-import React from "react";
-
+import "./form.scss";
+import EmojiPickerInput from "../emojiPicker/EmojiPickerInput.jsx";
 export default function Form() {
-  return <div>Form</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="prenom">Prénom</label>
+      <input type="text" name="prenom" id="prenom" />
+      <label htmlFor="nom">Nom</label>
+      <input type="text" name="nom" id="nom" />
+      <EmojiPickerInput />
+      <button className="btn">Envoyer</button>
+    </form>
+  );
 }
